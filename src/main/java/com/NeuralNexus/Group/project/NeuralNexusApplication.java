f352b6e7f3fd628ad217b43acaaed98c6d763189
Jai14 <jai.sridharanmurali@ucdconnect.ie>
2023-03-15 23:28:26 +0000

    setup

---
+++ src/main/java/com/NeuralNexus/Group/project/NeuralNexusApplication.java
@@ -0,0 +1,13 @@
+package com.NeuralNexus.Group.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NeuralNexus/Group/project/NeuralNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NeuralNexus.Group.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
