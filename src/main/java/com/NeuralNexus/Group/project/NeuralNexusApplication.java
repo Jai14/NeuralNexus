de01226fecd253b5422cc37b4cd08fc52e2eaea8
dhruvg102 <dhruv.gupta1500@gmail.com>
2023-03-16 04:40:36 +0000

    Main Page + random resources

--- src/main/java/com/NeuralNexus/Group/project/NeuralNexusApplication.java
+++ src/main/java/com/NeuralNexus/Group/project/NeuralNexusApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NeuralNexusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NeuralNexusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NeuralNexusApplication.class, args);
+    }
 
 }
